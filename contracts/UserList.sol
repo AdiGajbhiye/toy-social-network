444800709efa88c6afca08b4ab090d340cd7ff50
Aditya <aditya.gajbhiye.20@gmail.com>
2020-12-17 22:52:46 +0530

    added update user and test

--- contracts/UserList.sol
+++ contracts/UserList.sol
@@ -9,7 +9,13 @@ contract UserList {
         string description;
     }
 
-    event UserCreated(uint256 id, string name, uint256 age, string description);
+    event UserUpserted(
+        uint256 id,
+        string name,
+        uint256 age,
+        string description
+    );
+
     uint256 public userCount = 0;
     mapping(uint256 => User) public users;
 
@@ -20,6 +26,20 @@ contract UserList {
     ) public {
         userCount++;
         users[userCount] = User(userCount, _name, _age, _description);
-        emit UserCreated(userCount, _name, _age, _description);
+        emit UserUpserted(userCount, _name, _age, _description);
+    }
+
+    function updateUser(
+        uint256 _id,
+        string memory _name,
+        uint256 _age,
+        string memory _description
+    ) public {
+        User memory _user = users[_id];
+        _user.name = _name;
+        _user.age = _age;
+        _user.description = _description;
+        users[_id] = _user;
+        emit UserUpserted(userCount, _name, _age, _description);
     }
 }
--- test/UserList.test.js
+++ test/UserList.test.js
@@ -25,4 +25,13 @@ contract("UserList", () => {
     assert.equal(event.age, 12);
     assert.equal(event.description, "qwe");
   });
+
+  it("update user", async () => {
+    const result = await userList.updateUser(1, "asd", 18, "qwe");
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1);
+    assert.equal(event.name, "asd");
+    assert.equal(event.age, 18);
+    assert.equal(event.description, "qwe");
+  });
 });
