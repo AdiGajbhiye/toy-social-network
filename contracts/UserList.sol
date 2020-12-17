996d436dcaea35f736d3bdd9c174d3989a695576
Aditya <aditya.gajbhiye.20@gmail.com>
2020-12-17 22:45:46 +0530

    added user and tests

--- contracts/UserList.sol
+++ contracts/UserList.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.5.16;
 
+
 contract UserList {
-    uint public userCount = 0;
+    struct User {
+        uint256 id;
+        string name;
+        uint256 age;
+        string description;
+    }
+
+    event UserCreated(uint256 id, string name, uint256 age, string description);
+    uint256 public userCount = 0;
+    mapping(uint256 => User) public users;
+
+    function createUser(
+        string memory _name,
+        uint256 _age,
+        string memory _description
+    ) public {
+        userCount++;
+        users[userCount] = User(userCount, _name, _age, _description);
+        emit UserCreated(userCount, _name, _age, _description);
+    }
 }
---
+++ test/UserList.test.js
@@ -0,0 +1,28 @@
+const UserList = artifacts.require("./UserList.sol");
+
+contract("UserList", () => {
+  let userList;
+
+  before(async () => {
+    userList = await UserList.deployed();
+  });
+
+  it("deploys successfully", async () => {
+    const address = await userList.address;
+    assert.notEqual(address, 0x0);
+    assert.notEqual(address, "");
+    assert.notEqual(address, null);
+    assert.notEqual(address, undefined);
+  });
+
+  it("create user", async () => {
+    const userCount = await userList.userCount();
+    assert.equal(userCount.toNumber(), 0);
+    const result = await userList.createUser("asd", 12, "qwe");
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1);
+    assert.equal(event.name, "asd");
+    assert.equal(event.age, 12);
+    assert.equal(event.description, "qwe");
+  });
+});
